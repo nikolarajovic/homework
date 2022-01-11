46f1aca1236dfe2d63268321594ad2fefe514fb5
Nikola Rajovic <nikola.rajovic1992@gmail.com>
2022-01-11 17:02:07 +0100

    Class smart air conditioning code grooming

--- src/d10_12_2022/SmartAirConditioning.java
+++ src/d10_12_2022/SmartAirConditioning.java
@@ -5,18 +5,18 @@ public class SmartAirConditioning {
 	String brand;
 	int temp;
 	String mode;
-	
+
 	public void print() {
-		
-		if (this.temp >= 16 && this.temp <= 35) {
-		System.out.println("*** " + this.brand + " ***");
-		System.out.println("Temperature is set at " + this.temp + " degrees.");
-		System.out.println("Current mode: " + this.mode);
+
+		if (this.temp >= 16 && this.temp <= 35 && this.mode.equals("Winter") || this.mode.equals("Summer")) {
+			System.out.println("*** " + this.brand + " ***");
+			System.out.println("Temperature is set at " + this.temp + " degrees.");
+			System.out.println("Current mode: " + this.mode);
 		} else {
 			System.out.println("*** " + this.brand + " ***");
-			System.out.println("Invalid temperature.");
+			System.out.println("Invalid mode or temperature.");
 			System.out.println("********************");
 		}
 	}
-	
+
 }
